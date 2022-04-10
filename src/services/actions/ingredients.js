e365e5384e046f27f54836045ddda26dd3b0efcb
Valentiva Davydova <81330170+Wunshh@users.noreply.github.com>
2022-04-10 23:13:46 +0300

    add actions

--- src/services/actions/ingredients.js
+++ src/services/actions/ingredients.js
@@ -1,18 +1,32 @@
 import * as api from '../../utils/api';
 
 const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
+const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
+const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED';
 
 function getIngredientsData() {
     return function(dispatch) {
+        dispatch({
+            type: GET_INGREDIENTS_REQUEST
+        });
         api.getIngredientsData()
         .then((res) => {
-            dispatch({
-                type: GET_INGREDIENTS_SUCCESS,
-                allIngredients: res.data
-            })
+            if (res) {
+                dispatch({
+                    type: GET_INGREDIENTS_SUCCESS,
+                    allIngredients: res.data
+                })
+            } else {
+                dispatch({
+                    type: GET_INGREDIENTS_FAILED
+                });
+            }
         })
         .catch((err) => {
             console.log(err);
+            dispatch({
+                type: GET_INGREDIENTS_FAILED
+            });
         });
     }
 }
--- src/services/actions/order.js
+++ src/services/actions/order.js
@@ -1,18 +1,32 @@
 import * as api from '../../utils/api';
 
 const SEND_ORDER_SUCCESS = 'SEND_ORDER_SUCCESS';
+const SEND_ORDER_REQUEST = 'SEND_ORDER_REQUEST';
+const SEND_ORDER_FAILED = 'SEND_ORDER_FAILED';
 
 const sendOrder = (itemsId) => {
     return function(dispatch) {
+        dispatch({
+            type: SEND_ORDER_REQUEST
+        });
         api.sendOrder(itemsId) 
         .then((res) => {
-            dispatch({
-                type: SEND_ORDER_SUCCESS,
-                order: res
-            })
+            if (res) {
+                dispatch({
+                    type: SEND_ORDER_SUCCESS,
+                    order: res
+                }); 
+            } else {
+                dispatch({
+                    type: SEND_ORDER_FAILED
+                });
+            }
         })
         .catch((err) => {
             console.log(err);
+            dispatch({
+                type: SEND_ORDER_FAILED
+            });
         });
     }
 }
